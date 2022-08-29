2b10e79e2ff32a81311f8046c8d77063832e0d7f
Pranit Das <pranit@Pranits-MacBook-Air.local>
2022-08-30 01:03:30 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,8 +8,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
+    // bringVegetables(rupees: 50);
+    // bringVegetables(thaila: true);
     // int days = 30;
     // String name = "Pranit Das";
     // double pi = 3.14;
@@ -19,7 +21,26 @@ class MyApp extends StatelessWidget {
     // const pi = 3.14;
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
+
+  // bringVegetables({ @required bool thaila = false, int rupees = 100})
+  // {
+  //   //Take Cycle
+
+  //   // Go to Sector 16
+  // }
+
 }
