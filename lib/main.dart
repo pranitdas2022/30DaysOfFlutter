ebaf1ad0d2462c7bbb1a8323247fa122d683c92b
Pranit Das <pranit@Pranits-MacBook-Air.local>
2022-08-30 00:05:09 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
+    // int days = 30;
+    // String name = "Pranit Das";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = "Tuesday";
+    // const pi = 3.14;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
