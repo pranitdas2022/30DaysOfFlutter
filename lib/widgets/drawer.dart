85b1e00126d99baa65f5ebeb5e5f691f7e348017
Pranit Das <pranit@Pranits-MacBook-Air.local>
2022-08-30 06:26:34 +0530

    Day 9 completed

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://scontent.fccu27-1.fna.fbcdn.net/v/t39.30808-6/246360320_1679958512395455_2886383345989126734_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=QbHg0gCaxnAAX8vY2vg&_nc_ht=scontent.fccu27-1.fna&oh=00_AT95B8CiFZTRIbgPHycXw95HW834hNCm-H7P8ZmdqkBu6g&oe=6311C84A";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("Pranit Das"),
+                accountEmail: Text("pranitdas09@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
